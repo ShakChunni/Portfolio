35b9af752fb73d522fd32d0419329ca3a20b9855
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-05-03 18:32:38 +0600

    first commit

---
+++ assets/js/assets.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const assets = () => {
+  return (
+    <View>
+      <Text>assets</Text>
+    </View>
+  )
+}
+
+export default assets
+
+const styles = StyleSheet.create({})

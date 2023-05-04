b33168fe7f2373c9bd9bd205007ebbe18071684f
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-05-04 16:42:27 +0600

    first commit

---
+++ assets/js/etc.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const etc = () => {
+  return (
+    <View>
+      <Text>etc</Text>
+    </View>
+  )
+}
+
+export default etc
+
+const styles = StyleSheet.create({})

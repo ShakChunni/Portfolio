c7c2909d1a3249f218b52ee883a17bf7799229f1
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-05-08 16:07:31 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const app = () => {
+  return (
+    <View>
+      <Text>app</Text>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({})
